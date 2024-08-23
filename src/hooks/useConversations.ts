4fcbf7aab1cc64b5730b4259897e4482b68aecda
Jamal Soueidan <jamal@soueidan.com>
2024-08-23 16:35:53 +0200

    remove user from useConversations

--- src/hooks/useConversations.ts
+++ src/hooks/useConversations.ts
@@ -1,30 +1,11 @@
 import { useQuery } from "@realm/react";
-import {
-  Conversation,
-  ConversationSchema,
-  User,
-  UserSchema,
-} from "../models/data";
-import { useRealmUser } from "./useRealmUser";
+import { Conversation, ConversationSchema } from "../models/data";
 
 export function useConversations() {
-  const user = useRealmUser();
-
-  const myUser = useQuery<User>(
-    UserSchema.name,
-    (collection) => {
-      return collection.filtered("user_id = $0", user?.id);
-    },
-    []
-  );
-
   const conversations = useQuery<Conversation>(
     ConversationSchema.name,
-    (collection) =>
-      collection
-        .filtered("hidden == false OR hidden == null")
-        .sorted("timestamp", true),
-    [myUser[0].updated_at]
+    (collection) => collection.sorted("timestamp", true),
+    []
   );
 
   return {
